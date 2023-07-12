ccb1c2cf1dc2f01f1f6d29e6387986dabed5e060
Suhail Alhassanat <108057779+suhail-alhassanat@users.noreply.github.com>
2023-07-12 22:38:20 +0300

    Delete jest.config.ts

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from '@jest/types';
-// Explanation: This file just for configure Typescript tests and transform it to Commonjs code
-const config: Config.InitialOptions = {
-  verbose: true,
-  transform: {
-    '^.+\\.(ts)?$': 'ts-jest',
-  },
-};
-export default config;
